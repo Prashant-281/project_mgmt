2a876edad7b17aab7357f8a8a7b80ee0a6180c65
Prashant <prashantkrsharma281@gmail.com>
2025-09-30 10:08:00 +0530

    some fixes

--- project_mgmt_backend/src/config/mongoDB.ts
+++ project_mgmt_backend/src/config/mongoDB.ts
@@ -6,7 +6,7 @@ const connectDB = async (mongoUri: string) => {
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    process.exit(1); // Exit process with failure
+    process.exit(1); 
   }
 };
 
--- project_mgmt_backend/src/server.ts
+++ project_mgmt_backend/src/server.ts
@@ -4,9 +4,6 @@ import app from './app';
 import connectDB from './config/mongoDB';
 import {PORT, JWT_SECRET, JWT_EXPIRES_IN, DB_URI} from "../src/config/env";
 
-console.log('JWT_SECRET:', JWT_SECRET);
-console.log('JWT_EXPIRES_IN:', JWT_EXPIRES_IN);
-
 connectDB(DB_URI as string).then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
