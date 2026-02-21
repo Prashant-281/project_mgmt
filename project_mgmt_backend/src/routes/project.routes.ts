3d2cb64731e5155f1e00ed9a924b7cb448a55eee
Prashant <prashantkrsharma281@gmail.com>
2026-02-21 16:37:16 +0530

    key changes

--- project_mgmt_backend/src/routes/project.routes.ts
+++ project_mgmt_backend/src/routes/project.routes.ts
@@ -1,19 +1,42 @@
-import express from 'express';
+import express from "express";
 import {
   createProject,
   deleteProject,
   getAllProjects,
   updateProject,
-} from '../controllers/project.controller';
-import authorise from '../middlewares/authorise';
-import taskRoutes from './task.routes';
+} from "../controllers/project.controller";
+import authorise from "../middlewares/authorise";
+import taskRoutes from "./task.routes";
+import { body } from "express-validator";
 const router = express.Router();
 
 router.use(authorise);
 //api/v1/projects/...
-router.post('/', createProject);
-router.get('/', getAllProjects);
-router.put('/:id', updateProject);
-router.delete('/:id', deleteProject);
-router.use('/:projectId/tasks', taskRoutes);
+router.post(
+  "/",
+  [
+    body("title")
+      .isLength({ max: 20 })
+      .notEmpty()
+      .withMessage("The title should not be more than 20 characters"),
+    body("name").notEmpty(),
+    body("description").notEmpty().withMessage("Title should not be empty"),
+  ],
+  createProject
+);
+router.get("/", getAllProjects);
+router.put(
+  "/:id",
+  [
+    body("title")
+      .notEmpty()
+      .isLength({ max: 10 })
+      .withMessage("The title should not be more than 20 characters"),
+    body("name").notEmpty(),
+    body("description").notEmpty().withMessage("Title should not be empty"),
+  ],
+  updateProject
+);
+router.delete("/:id", deleteProject);
+router.use("/:projectId/tasks", taskRoutes);
 export default router;
